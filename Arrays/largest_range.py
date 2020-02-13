7931f9da21b273cabe1474b6f3084994e35e127a
Maxgrid <xlxmht@gmail.com>
2020-02-13 12:31:04 +0530

    largest range implemented

---
+++ Arrays/largest_range.py
@@ -0,0 +1,31 @@
+# O(N)
+def largest_range(array):
+    visited = {}
+    longest_length = 0
+    best_range = []
+    for num in array:
+        visited[num] = True
+    for num in array:
+        if not visited[num]:
+            continue
+        visited[num] = False
+        current_length = 1
+        left = num - 1
+        right = num + 1
+        while left in visited:
+            current_length += 1
+            left -= 1
+        while right in visited:
+            current_length += 1
+            right += 1
+        if current_length > longest_length:
+            longest_length = current_length
+            best_range = [left + 1, right - 1]
+    return best_range
+
+
+# test_array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]
+# test_array = [4, 2, 1, 3, 6]
+test_array = [1]
+result = largest_range(test_array)
+print(result)
--- Arrays/subarray_sort.py
+++ Arrays/subarray_sort.py
@@ -1,32 +1,32 @@
 def subarray_sort(array):
-    min = float("inf")
-    max = float("-inf")
+    min_val = float("inf")
+    max_val = float("-inf")
     for index in range(len(array)):
         if not _is_out_of_order(index, array):
-            result = _set_min_max(index, min, max, array)
-            min = result[0]
-            max = result[1]
-    return _find_place(min, max, array)
+            min_max_array = _set_min_max(index, min_val, max_val, array)
+            min_val = min_max_array[0]
+            max_val = min_max_array[1]
+    return _find_place(min_val, max_val, array)
 
 
-def _find_place(min, max, array):
-    if min == float("inf"):
+def _find_place(min_val, max_val, array):
+    if min_val == float("inf"):
         return [-1, -1]
     left_sub_array_idx = 0
     right_sub_array_idx = len(array) - 1
-    while min >= array[left_sub_array_idx]:
+    while min_val >= array[left_sub_array_idx]:
         left_sub_array_idx += 1
-    while max <= array[right_sub_array_idx]:
+    while max_val <= array[right_sub_array_idx]:
         right_sub_array_idx -= 1
     return [left_sub_array_idx, right_sub_array_idx]
 
 
-def _set_min_max(index, mn, mx, array):
-    if array[index] >= mx:
-        mx = array[index]
-    elif array[index] <= mn:
-        mn = array[index]
-    return [mn, mx]
+def _set_min_max(index, min_val, max_val, array):
+    if array[index] >= max_val:
+        max_val = array[index]
+    elif array[index] <= min_val:
+        min_val = array[index]
+    return [min_val, max_val]
 
 
 def _is_out_of_order(index, array):
@@ -35,10 +35,15 @@ def _is_out_of_order(index, array):
     elif index == len(array) - 1:
         return array[index] > array[index - 1]
     else:
-        return array[index] > array[index - 1] and array[index] < array[index + 1]
+        return array[index - 1] < array[index] < array[index + 1]
 
 
-# test_array = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
-test_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+test_array = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]  # Expected Output: [3, 9]
+# test_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] # Expected Output: [-1, -1]
 result = subarray_sort(test_array)
 print(result)
+
+# scan whole array and find elements that are not in order
+# select min and max while marking those elements as out of order
+# Iterate array again and find place(index) for min and max to be put in
+# Hence finding resulting sub-array
